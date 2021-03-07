0f3f47ca36caa8a35df48144bffdea2ae2b9c409
achilles2200 <ace29703@outlook.com>
2021-03-07 09:59:11 -0500

    commiting using eclipse

---
+++ chaper2HelloJava/src/chaper2HelloJava/HelloJava.java
@@ -0,0 +1,18 @@
+package chaper2HelloJava;
+
+import javax.swing.*;
+
+public class HelloJava {
+
+	public static void main(String[] args) {
+		//System.out.println("Hello, Java!");
+		JFrame frame = new JFrame("Hello, Java!");
+		JLabel label = new JLabel("Hello, Java!", JLabel.CENTER);
+		frame.add(new HelloComponent());
+		//frame.add(label);
+		frame.setSize(300,300);
+		frame.setVisible(true);
+		
+	}
+
+}
